4c3f5c783774bfa5873ada8b36a46a8e9786ee3f
Kevin <k.kochpatcharin@gmail.com>
2017-11-25 02:01:35 -0800

    Undo this later. Will potentially lose all data. Think of this as a backup.

--- gotaspot-v0.1/src/Registration.js
+++ gotaspot-v0.1/src/Registration.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Login extends Component {
+class Registration extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -15,18 +15,26 @@ class Login extends Component {
                 <h3>User name:</h3>
                 <br/>
                 <input type="text" name="username" onChange = {(event,newValue) => this.setState({username:newValue})}/>
+                {this.usernameAvailable()}
                 <br/>
                 <h3>User password:</h3>
                 <br/>
                 <input type="password" name="psw" onChange = {(event,newValue) => this.setState({password:newValue})}/>
                 <br/>
+                <input type = "password" name = "cfm" onChange = {(event,newValue) => this.verifyPassword(event,newValue)}/>
+                <br/>
                 <input type="submit" onClick={(event) => this.handleClick(event)}/>
             </div>
         );
     }
+    verifyPassword(event,newValue) {
+        
+    }
+    usernameAvailable() {
+        return;
+    }
     handleClick(event) {
         // something about firebase login stuff here???????
         // IM SO CONFUSED WHAT ARE WE EVEN WORKING ON
-        // have firebase deal with stuff
     }
 }
