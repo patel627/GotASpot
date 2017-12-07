618ea0f8c1ab2ed47fda90e7d9d28cb8fce01769
patel627 <patel627@purdue.eduL>
2017-12-06 23:23:47 -0500

    starting to add in elements for login and data entry

--- src/App.js
+++ src/App.js
@@ -4,6 +4,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      address: '',
+      description:'',
+      username:'',
+      items:[],
+
+      user: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.hangleSubmit = this.handleSubmit.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+
+
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  login() {
+    auth.signInWithPopup(provider).then((result) => {
+      const user = result.user;
+      this.setState({user});
+    });
+  }
+
+  logout() {
+    auth.signOut().then(() => {
+      this.setState({user: null});
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    firebaseutil.addSpot("userid1", this.state.address, this.state.description, 4, 5, this.state.user.displayName ||  this.state.user.email, ["good", "bad"]);
+    
+    this.setState({
+      address: '',
+      username: '',
+      description: ''
+    });
+ 
+  }
+
   render() {
     return (
       <div className="App">
