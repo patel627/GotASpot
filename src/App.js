3f5ee93973eac53d41e5a0b40dff53293025d2c5
patel627 <patel627@purdue.eduL>
2017-12-07 03:16:30 -0500

    checking again

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
     auth.onAuthStateChanged((user) => {
       if (user)
         this.setState({ user });
-      console.log("USER CHECK: " + user);
+      console.log("USER CHECK: " + user.toString());
     });
 
     /*const spotsRef = firebase.database().ref('ParkingSpots');
